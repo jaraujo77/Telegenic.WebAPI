caa0b6839ff39363a6b1e8677be871f3f86056a1
jorge_araujo <araujojr.jorge@gmail.com>
2020-06-01 22:59:05 -0500

    Added controllers and stubbed out Series

--- Telegenic.WebAPI/App_Start/WebApiConfig.cs
+++ Telegenic.WebAPI/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using Castle.Windsor.Installer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using Telegenic.WebAPI.Plumbing.CastleWindsor;
 
@@ -14,6 +15,7 @@ namespace Telegenic.WebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
